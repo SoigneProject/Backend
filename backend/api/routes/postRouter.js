2ea9f4b5af767cdf0eacc4e89114e03f224dac38
Anthony Jhong <anthonyjhong@gmail.com>
2019-10-06 18:48:17 -0700

    Update postRouter.js

--- backend/api/routes/postRouter.js
+++ backend/api/routes/postRouter.js
@@ -4,5 +4,7 @@ module.exports = function (app) {
 
     // app.route('/posts').get(post_controller.get_all_posts_by_user);
     app.route('/posts').get(post_controller.get_Post_with_id);
-    app.route('posts').post(post_controller.create_Post);
+    app.route('/posts').post(post_controller.create_Post);
+    app.route('/posts').delete(post_controller.delete_post_with_id);
 };
+
