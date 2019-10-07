56bc153a8b08ff1b406d342258ebdf7b601e54f2
Anthony Jhong <anthonyjhong@gmail.com>
2019-10-06 18:32:37 -0700

    Update postRouter.js

--- backend/api/routes/postRouter.js
+++ backend/api/routes/postRouter.js
@@ -1,8 +1,8 @@
 module.exports = function (app) {
 
-    var post_controller = require('../controllers/postController'); 
+    var user_controller = require('../controllers/postController'); 
 
-    app.route('/posts').get(post_controller.get_post_with_id);
-
-
+    app.route('/posts').get(post_controller.get_all_posts_by_user);
+    app.route('/posts').get(post_controller.get_Post_with_id);
+    app.route('posts').post(post_controller.create_Post);
 };
